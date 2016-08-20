4309ed1cca943317e6ddfc20d405d78ac5e7e4db
James Ruiz <fallen@Jamess-MacBook-Air.local>
2016-08-20 16:41:06 -0400

    Init Commit (so original)

---
+++ src/app/DeathStar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+using static Newtonsoft.Json.JsonConvert;
+
+namespace BattleSimulator
+{
+    public static class DeathStar
+    {
+        public static void Main(string[] args)
+        {
+            int kyloRenPower = RememberTheName(new Random(Guid.NewGuid().GetHashCode()).Next(1,201), 70, 10, 100);
+            int pikachuPower = RememberTheName(new Random(Guid.NewGuid().GetHashCode()).Next(1,201), 30, 40, 0);
+
+            WriteLine($"Pikachu's power is: {pikachuPower} units.");
+            WriteLine($"Kylo's power is: {kyloRenPower} units.");
+            WriteLine($"Kylo Ren obliterated Pikachu into {kyloRenPower-pikachuPower} space particles.");
+        }
+
+        private static int RememberTheName(int luck, int skill, int pleasure, int pain) =>
+            DeserializeObject<int>($"{luck + skill + pleasure + pain}");
+    }
+}
